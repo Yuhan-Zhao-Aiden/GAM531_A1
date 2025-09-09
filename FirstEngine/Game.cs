20c0502b070ab526ece6f2b00a421b7939449935
Yuhan Zhao <aidenzhao@hotmail.com>
2025-09-09 19:50:17 -0400

    Finish A1, add Documentation

--- FirstEngine/Game.cs
+++ FirstEngine/Game.cs
@@ -11,6 +11,7 @@ namespace FirstEngine
   {
     private int vbo;
     private int vao;
+    private int ebo;
     private int shaderProgramHandle;
     private static NativeWindowSettings native = new()
     {
@@ -20,19 +21,146 @@ namespace FirstEngine
     };
 
     public Game()
-    : base(GameWindowSettings.Default, native)
+    : base(GameWindowSettings.Default, native) // base constructor
     {
       this.Size = new Vector2i(1280, 720);
       this.CenterWindow(this.Size);
     }
 
+    protected override void OnFramebufferResize
+    (FramebufferResizeEventArgs e)
+    {
+      base.OnFramebufferResize(e);
+      GL.Viewport(0, 0, e.Width, e.Height);
+    }
+
     protected override void OnLoad()
     {
       base.OnLoad();
-      GL.ClearColor(new Color4(0.2f, 0.6f, 0.7f, 1f));
+      GL.ClearColor(new Color4(0.8f, 0.6f, 0.2f, 1f));
 
       // GL viewport size
-      
+      var fbSize = FramebufferSize;
+      GL.Viewport(0, 0, fbSize.X, fbSize.Y);
+
+      // vertex positions (4 vertex)
+      float[] vertices = new float[]
+      {
+        -0.5f, 0.5f, 0.0f, //0
+        0.5f, 0.5f, 0.0f, //1
+        -0.5f, -0.5f, 0.0f, //2
+        0.5f, -0.5f, 0.0f, //3
+      };
+
+      uint[] indices = {
+        0, 1, 2, // First triangle
+        1, 2, 3 // Second triangle
+      };
+
+      // VBO
+      vbo = GL.GenBuffer();
+      GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+      GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+      // VAO
+      vao = GL.GenVertexArray();
+      GL.BindVertexArray(vao);
+
+      // EBO - for 2 triangles
+      ebo = GL.GenBuffer();
+      GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+      GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+
+      GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+      GL.EnableVertexAttribArray(0);
+
+      // unbind
+      GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+      GL.BindVertexArray(0);
+
+      string vertexShaderCode = @"
+        #version 330 core
+        layout(location = 0) in vec3 aPosition;
+
+        void main()
+        {
+          gl_Position = vec4(aPosition, 1.0);
+        }
+      ";
+
+      string fragmentShaderCode = @"
+        #version 330 core
+        out vec4 FragColor;
+
+        void main()
+        {
+          FragColor = vec4(0.2, 0.5, 0.8, 1.0);
+        }
+      ";
+
+      int vShaderHandle = GL.CreateShader(ShaderType.VertexShader);
+      GL.ShaderSource(vShaderHandle, vertexShaderCode);
+      GL.CompileShader(vShaderHandle);
+      CheckShaderCompile(vShaderHandle, "Vertex Shader");
+
+      int fShaderHandle = GL.CreateShader(ShaderType.FragmentShader);
+      GL.ShaderSource(fShaderHandle, fragmentShaderCode);
+      GL.CompileShader(fShaderHandle);
+      CheckShaderCompile(fShaderHandle, "Fragment Shader");
+
+      shaderProgramHandle = GL.CreateProgram();
+      GL.AttachShader(shaderProgramHandle, vShaderHandle);
+      GL.AttachShader(shaderProgramHandle, fShaderHandle);
+      GL.LinkProgram(shaderProgramHandle);
+
+      GL.DetachShader(shaderProgramHandle, vShaderHandle);
+      GL.DetachShader(shaderProgramHandle, fShaderHandle);
+      GL.DeleteShader(vShaderHandle);
+      GL.DeleteShader(fShaderHandle);
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+      base.OnUpdateFrame(args);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+      base.OnRenderFrame(args);
+      GL.Clear(ClearBufferMask.ColorBufferBit);
+      GL.UseProgram(shaderProgramHandle);
+      GL.BindVertexArray(vao);
+      // Draw element using ebo
+      GL.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, 0);
+
+      SwapBuffers();
+    }
+
+    protected override void OnUnload()
+    {
+      // Unbind and delete buffers and shader program
+      GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+      GL.DeleteBuffer(vbo);
+
+      GL.BindVertexArray(0);
+      GL.DeleteVertexArray(vao);
+
+      GL.DeleteBuffer(ebo);
+
+      GL.UseProgram(0);
+      GL.DeleteProgram(shaderProgramHandle);
+
+      base.OnUnload();
+    }
+
+    private void CheckShaderCompile(int shaderHandle, string shaderName)
+    {
+      GL.GetShader(shaderHandle, ShaderParameter.CompileStatus, out int success);
+      if (success == 0)
+      {
+        string infoLog = GL.GetShaderInfoLog(shaderHandle);
+        Console.WriteLine($"Error compiling {shaderName}: {infoLog}");
+      }
     }
   }
 }
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# FirstRender
+# FirstRender OpenGL (C# + OpenTK)
+
+## Library
+OpenTK is used in this assignment for cross-platform
+
+## How to Run
+
+### Prerequisites
+- [.NET 8 SDK](https://dotnet.microsoft.com/en-us/)
+
+### Steps
+```bash
+# clone the project
+git clone https://github.com/Yuhan-Zhao-Aiden/GAM531_A1.git
+cd GAM531_A1
+
+# run
+dotnet run --project FirstEngine
+```
+
+## Extra Notes
+- The rectangle is drawn by render 2 triangles with VBO, VAO and EBO
+- EBO is a GPU buffer of index of vertices that tells GPU which vertices for each triangle
